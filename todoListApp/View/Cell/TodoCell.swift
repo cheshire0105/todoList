5b3e3a84b85c7e5041e54c1d5636964166e6e603
cheshire0105 <cheshire0105@gmail.com>
2023-09-19 19:03:58 +0900

    간헐적으로 셀이 동시에 눌리는 문제 해결

--- todoListApp/View/Cell/TodoCell.swift
+++ todoListApp/View/Cell/TodoCell.swift
@@ -9,9 +9,15 @@ import UIKit
 
 class TodoCell: UITableViewCell {
     
+    @IBOutlet weak var switchControl: UISwitch!
     @IBOutlet weak var cellLabel: UILabel!
     
     var indexPath: IndexPath?
+
+    func configure(with todo: Todo) {
+            cellLabel.text = todo.title
+        switchControl.isOn = todo.isCompleted // switchControl은 스위치의 IBOutlet이어야 합니다.
+        }
     
     override func awakeFromNib() {
         super.awakeFromNib()
